fce7090c479b9ff0d8dcfd30a1b43a182a9d25a8
Rmcstt <anisio.nanato@hotmail.com>
2026-02-18 23:40:22 -0300

    predict.py

--- src/model/__init__.py
+++ src/model/__init__.py
@@ -6,17 +6,27 @@ Este módulo contém funções para:
 - preprocessing: Pré-processamento de dados
 - train: Treinamento e seleção de modelos
 - evaluate: Avaliação de modelos
+- predict: Inferência e predições (para integração com API)
 """
 
 from .feature_engineering import build_features, create_target_column
 from .preprocessing import build_preprocessor
 from .train import train_model
 from .evaluate import evaluate_model
+from .predict import ModelPredictor, get_predictor, predict
 
 __all__ = [
+    # Feature Engineering
     "build_features",
     "create_target_column",
+    # Preprocessing
     "build_preprocessor",
+    # Training
     "train_model",
+    # Evaluation
     "evaluate_model",
+    # Prediction (API integration)
+    "ModelPredictor",
+    "get_predictor",
+    "predict",
 ]
